4e634a6308be0385cf97be8cd20b6ba8c7514557
angelv18 <angelvasquez0918@gmail.com>
2022-11-03 19:22:05 -0400

    changed and updated carousel

--- portfolio.js
+++ portfolio.js
@@ -22,11 +22,71 @@ $(() => {
         $(window).scroll(function() {
             stickyNav();
         });
-    });
-   
 
+});
+//Stack overflow for carousel
+jQuery(document).ready(function ($) {
+
+    var interval;
+    interval = setInterval(function () {
+      moveRight();
+    }, 3000);
+  
+    $('._slider').mouseover(function(){
+      clearInterval(interval);
+    });
     
-})
+    $('._slider').mouseleave(function(){
+      interval = setInterval(function () {
+        moveRight();
+        }, 3000);
+    });
+    
+      let slideCount = $('._slider ul li').length;
+      let slideWidth = $('._slider ul li').width();
+      let slideHeight = $('._slider ul li').height();
+      let sliderUlWidth = slideCount * slideWidth;
+      
+      $('._slider').css({ width: slideWidth, height: slideHeight });
+      
+      $('._slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+      
+      $('._slider ul li:last-child').prependTo('._slider ul');
+  
+      function moveLeft() {
+          $('._slider ul').animate({
+              left: + slideWidth
+          }, 200, function () {
+              $('._slider ul li:last-child').prependTo('._slider ul');
+              $('._slider ul').css('left', '');
+          });
+      };
+  
+      function moveRight() {
+          $('._slider ul').animate({
+              left: - slideWidth
+          }, 200, function () {
+              $('._slider ul li:first-child').appendTo('._slider ul');
+              $('._slider ul').css('left', '');
+          });
+      };
+  
+      $('._slider_prev').click(function () {
+          moveLeft();
+          return false;
+      });
+  
+      $('._slider_next').click(function () {
+          moveRight();
+          return false;
+      });
+  
+  });    
+  
+  
+    
+
+});
 
 
 
